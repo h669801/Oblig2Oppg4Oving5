407c177d6659a5212cc80dd9a79e724837977179
thoma <thoma@158.37.198.8>
2023-03-01 10:39:31 +0100

    oblig2oppg4oving5 ferdig

---
+++ Oblig2oppg4/src/oppgA/Main.java
@@ -0,0 +1,11 @@
+package oppgA;
+
+public class Main {
+
+	public static void main(String[] args) {
+        int n = 3;
+        int sum = SumN.sum(n);
+        System.out.println("Summen av de første " + n + "naturlige tallene er " + sum);
+    }
+
+}
